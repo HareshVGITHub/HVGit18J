a7b84fd346318bd5532f34f3f5541545ee1efd9d
Haresh Vaity <vaityharesh@gmail.com>
2021-06-17 22:19:18 +0530

    This my 3rd Commit

--- src/test/java/com/w2a/testcases/AddCustomerTest.java
+++ src/test/java/com/w2a/testcases/AddCustomerTest.java
@@ -19,6 +19,7 @@ public class AddCustomerTest extends TestBase {
 		
 		
 		//  This file has been changed for 2nd Commit
+		//  This is my 3rd Commit
 		if(!data.get("runmode").equals("Y")){
 			
 			throw new SkipException("Skipping the test case as the Run mode for data set is NO");
